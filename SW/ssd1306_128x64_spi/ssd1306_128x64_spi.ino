022e1823688bafff30e9288599b2ae083f99c75f
Ondřej Bureš <30869245+OBU5@users.noreply.github.com>
2021-02-27 13:23:09 +0100

    Added labels on MCU pins

---
+++ SW/ssd1306_128x64_spi/ssd1306_128x64_spi.ino
@@ -0,0 +1,99 @@
+/**************************************************************************
+ This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+ Pick one up today in the adafruit shop!
+ ------> http://www.adafruit.com/category/63_98
+
+ This example is for a 128x64 pixel display using SPI to communicate
+ 4 or 5 pins are required to interface.
+
+ Adafruit invests time and resources providing this open
+ source code, please support Adafruit and open-source
+ hardware by purchasing products from Adafruit!
+
+ Written by Limor Fried/Ladyada for Adafruit Industries,
+ with contributions from the open source community.
+ BSD license, check license.txt for more information
+ All text above, and the splash screen below must be
+ included in any redistribution.
+ **************************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for SSD1306 display connected using software SPI (default case):
+#define OLED_MOSI   23
+#define OLED_CLK   18
+#define OLED_DC    5
+#define OLED_CS    -1
+#define OLED_RESET 17
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
+  OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+/* Comment out above, uncomment this block to use hardware SPI
+#define OLED_DC     6
+#define OLED_CS     7
+#define OLED_RESET  8
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
+  &SPI, OLED_DC, OLED_RESET, OLED_CS);
+*/
+
+int hCenter = 32;                               // horizontal center of 64 / 2 = 32
+int Radius = 30;                                // radius of circle
+const float Pi = 3.14159265359;                 // Pi
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+void setup() {
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  } 
+}
+
+void loop(){
+
+  display.drawRect(0, 0, 120, 64, WHITE);                      // draws border    
+  int index = 0;
+  for(int j = 0; j < 10; j ++){
+    display.clearDisplay();                                      // clears display
+    for (int i=0; i<120; i++){                                   // draws 120 pixels per loop
+      index++;
+      float Angle = index * 10;                                       // 120 X 3 = 360°
+      int a = (hCenter + (sin(Angle * (Pi / 180)) * Radius));    // Pi/180 converts degrees to radians
+      int b = (hCenter + (cos(Angle * (Pi / 180)) * Radius));    // Pi/180 converts degrees to radians
+  
+      display.drawPixel(i,a,WHITE);                              // draws each pixel for Sine wave
+      display.display();                                         // displays new screen information
+      delay(50);
+    } 
+  }
+}
